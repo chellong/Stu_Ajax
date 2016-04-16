d2b0dd383c8abc19f7ff5d7ae3978ce39cab9cc4
晓龙 <352977335@qq.com>
2016-04-17 01:02:43 +0800

    16,04,17,00
    ajax简单应用

---
+++ AJax/src/com/example/ajax/beans/ShoppingCartItem.java
@@ -0,0 +1,36 @@
+package com.example.ajax.beans;
+
+public class ShoppingCartItem {
+
+	private int number;
+	
+	private String bookName;
+	
+	private float price;
+
+	public int getNumber() {
+		return number;
+	}
+
+	public void setNumber(int number) {
+		this.number = number;
+	}
+
+	public String getBookName() {
+		return bookName;
+	}
+
+	public void setBookName(String bookName) {
+		this.bookName = bookName;
+	}
+
+	public float getPrice() {
+		return price;
+	}
+
+	public void setPrice(float price) {
+		this.price = price;
+	}
+	
+	
+}
